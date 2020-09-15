ac3d285a4cfc9ad40b99e48b305e072f76ae1bf8
Torben Werner <torbensky@users.noreply.github.com>
2020-09-14 17:41:18 -0700

    NEW: blind image pairs fn
    Also, removed the old olig2draq5 mode because it won't be needed anymore

--- blind_image-or-pair.py
+++ blind_image-or-pair.py
@@ -24,29 +24,30 @@ def appendToBlindCSV(imagesDir, line):
 def generateBlindId(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def getDraqIds(imagesDir):
-    ids = []
-    for draqFile in glob.glob(os.path.join(imagesDir, "*Draq5*.tif")):
-        match = re.match(r'.*?Draq5_(.*?).tif', draqFile)
-        ids.append(match.group(1))
-    return ids
 
-def blindOligDraqImages(imagesDir):
-    appendToBlindCSV(imagesDir,'Draq5 File Source,Olig File Source,Original ID,Blind ID\n')
-    ids = getDraqIds(imagesDir)
-    print "Blinding %d image pairs..." % len(ids)
-    for id in ids:
+def groupImagesByPrefix(images):
+    groups = {}
+    for image in images:        
+        groupName = '_'.join(image.split('_')[:-1])
+        if groupName in groups:
+            groups[groupName].append(image)
+        else:
+            groups[groupName] = [image]
+    return groups
+
+def blindPrefixGroupedImages(imagesDir, extension):
+    allImages = glob.glob(os.path.join(imagesDir, "*.%s" % extension))
+    groups = groupImagesByPrefix(allImages)
+    for group in groups:
         blindId = generateBlindId()
-        oligFile = os.path.join(imagesDir, "Olig2_" + id + ".tif")
-        draqFile = os.path.join(imagesDir, "Olig2_Draq5_" + id + ".tif")
-        #print "\t%s -> %s" % (id, blindId)
-
         os.mkdir(os.path.join(imagesDir, blindId))
-        shutil.copyfile(oligFile, os.path.join(imagesDir, blindId, "Olig2.tif"))
-        shutil.copyfile(draqFile, os.path.join(imagesDir, blindId, "Olig2_Draq5.tif"))
-
-        appendToBlindCSV(imagesDir, "%s,%s,%s,%s\n" % (draqFile,oligFile,id,blindId))
-    print "All done!"
+        for imageFile in groups[group]:
+            # the "discriminator" is the non-unique part of the filename
+            # We need this to create the new, blinded file. We only blind the grouped part because that is where the identifying info is.
+            # The last part of the file name is just the type of image and does not identify the individual.
+            discriminator = imageFile.split('_')[-1]
+            shutil.copyfile(imageFile, os.path.join(imagesDir, blindId, "%s_%s" % (blindId,discriminator)))
+            appendToBlindCSV(imagesDir, "%s,%s\n" % (imageFile,blindId))
 
 def blindAllImages(imagesDir, extension):
     for imageFile in glob.glob(os.path.join(imagesDir, "*.%s" % extension)):
@@ -57,17 +58,17 @@ def blindAllImages(imagesDir, extension):
 
 parser = argparse.ArgumentParser(description='Blind a directory of images')
 parser.add_argument('input_dir', type=str, help='The path to a directory of images you want blinded')
-parser.add_argument('type', type=str, choices=['olig2-draq5-pairs', 'all-images-in-dir'], help='The type of blinding to use')
+parser.add_argument('type', type=str, choices=['all-images-in-dir', 'group-by-prefix'], help='The type of blinding to use')
 parser.add_argument('--fileType', type=str, dest='file_type', default="tif",
                     help='The type of image file to blind (default: "tif")')
 
 args = parser.parse_args()
 
-if args.type == 'olig2-draq5-pairs':
-    print "Blinding Olig2 Draq5 image pairs..."
-    blindOligDraqImages(args.input_dir)
-elif args.type == 'all-images-in-dir':
-    print "Blinding all images in directory %s..." % args.input_dir
+if args.type == 'all-images-in-dir':
+    print "Blinding all '.%s' images in directory %s..." % (args.file_type, args.input_dir)
     blindAllImages(args.input_dir, args.file_type)
+elif args.type == 'group-by-prefix':
+    print "Blinding groups of '.%s' images in directory %s..." % (args.file_type, args.input_dir)
+    blindPrefixGroupedImages(args.input_dir, args.file_type)
 else:
     print "Error: unrecognized 'type' argument - doing nothing :'("
